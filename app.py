1a782c69638ff8096d3f12777ac182bad5de7dfb
gautam bansal <bansalgautam21@gmail.com>
2022-07-08 10:45:09 +0530

    Final commit

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import functools
+import PaytmChecksum
+import requests
 import json
-from flask import Flask, jsonify, request,render_template,session,g,redirect,url_for
-from models import db,Users
+import uuid
+import json
+from flask import Flask, jsonify, request,render_template,session,g,redirect,url_for,flash
+from sqlalchemy import desc, false
+from models import db,Users,Bookings
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
@@ -75,6 +80,8 @@ def adminLogin():
             #storing user_id in the session
             #it will be used to check if admin is logged in
             session['user_id'] = user.id
+            if user.isAdmin is True:
+                return  redirect(url_for('adminDashboard'))    
             return  redirect(url_for('dashboard'))
         
         return render_template( 'adminLogin.html', msg='Wrong user or password', form=form)
@@ -103,6 +110,21 @@ def login_required(view):
 
     return wrapped_view
 
+# This function will check if logged in user is admin or not
+def admin_login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('adminLogin'))
+        user = Users.query.filter_by(id=g.user).first()
+        if user.isAdmin is False:
+            return redirect(url_for('adminLogin'))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
+
 #Logout feature
 
 @app.route('/logout')
@@ -112,12 +134,138 @@ def logout():
     return redirect(url_for('adminLogin'))
 
 
-@app.route('/home',methods = ['GET'])
+@app.route('/admin',methods = ['GET'])
+@admin_login_required
+@login_required
+def adminDashboard():
+    bookings = Bookings.query.filter_by(status=True).all()
+    return render_template('adminDashboard.html',bookings = bookings)
+
+@app.route('/home',methods = ['GET','POST'])
 @login_required
 def dashboard():
-    #forms = Forms.query.filter_by(user_id=g.user).order_by(desc(Forms.creation_date)).all() 
-    return render_template('dashboard.html')
+    bookings = Bookings.query.filter_by(user_id=g.user).order_by(desc(Bookings.booking_date)).all()
+    all_bookings = Bookings.query.filter_by(status=True).all()
+    tot = Bookings.query.filter_by(status=True).count()
+    num = 1
+    for booking in all_bookings:
+        if booking.user_id==g.user:
+            break
+        num+=1
+    latest_booking = Bookings.query.filter_by(user_id=g.user , status=True).first()
+    #print(num)
+    return render_template('dashboard.html',bookings = bookings,latest=latest_booking,turn = num,tot=tot)
 
+@app.route('/join',methods = ['GET','POST'])
+@login_required
+def joinQ():
+    
+    paytmParams = dict()
+    orderId = str(uuid.uuid1().hex)
+    print(orderId)
+    paytmParams["body"] = {
+        "requestType"   : "Payment",
+        "mid"           : "yqIOLs05230201544957",
+        "websiteName"   : "WEBSTAGING",
+        "orderId"       : orderId,
+        "callbackUrl"   : url_for('checkout'),
+        "txnAmount"     : {
+            "value"     : "100.00",
+            "currency"  : "INR",
+        },
+        "userInfo"      : {
+            "custId"    : g.user,
+        },
+    }
+
+    # Generate checksum by parameters we have in body
+    # Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys 
+    checksum = PaytmChecksum.generateSignature(json.dumps(paytmParams["body"]),"pRXhFwWqozggJi1J")
+
+    paytmParams["head"] = {
+        "signature"    : checksum
+    }
+
+    post_data = json.dumps(paytmParams)
+
+    # for Staging
+    url = "https://securegw-stage.paytm.in/theia/api/v1/initiateTransaction?mid=yqIOLs05230201544957&orderId="+orderId
+
+    # for Production
+    # url = "https://securegw.paytm.in/theia/api/v1/initiateTransaction?mid=YOUR_MID_HERE&orderId=ORDERID_98765"
+    response = requests.post(url, data = post_data, headers = {"Content-type": "application/json"}).json()
+    #print(response['body']['txnToken']) 
+    token = response['body']['txnToken']
+    return render_template('checkout.html',orderId=orderId,token=token)
+    
+
+@app.route('/checkout',methods = ['GET','POST'])
+@login_required
+def checkout():
+    paytmParams = dict()
+    paytmParams = request.form.to_dict()
+    print(paytmParams)
+    paytmChecksum = paytmParams['CHECKSUMHASH']
+    paytmParams.pop('CHECKSUMHASH', None)
+    isVerifySignature = PaytmChecksum.verifySignature(paytmParams, "pRXhFwWqozggJi1J",paytmChecksum)
+    
+    if isVerifySignature and paytmParams['RESPCODE']=='01':
+        try:
+            newBooking = Bookings()
+            newBooking.user_id = g.user 
+            newBooking.order_id = paytmParams['ORDERID']
+            newBooking.transaction_id = paytmParams['TXNID']
+            newBooking.amount = int(float(paytmParams['TXNAMOUNT']))
+            newBooking.txn_mode = "Online"
+            db.session.add(newBooking)
+            db.session.commit()
+
+        except Exception as e:
+            #initiate a refund
+            return render_template('fail.html',msg = "Any amount deducted will be credit back to your account soon." )
+            print(e)
+
+        return redirect(url_for('dashboard'))
+
+        
+    else:
+        return render_template('fail.html',msg = paytmParams['RESPMSG'] )
+    
+    
+
+@app.route('/deactive/<int:booking_id>',methods = ['GET','POST'])
+@admin_login_required
+@login_required
+def deactiveBooking(booking_id):
+    curr_booking = Bookings.query.get(booking_id)
+
+    curr_booking.status = False
+    try:
+        db.session.commit()
+        flash("success")
+    except:
+        flash("Bad Request")
+
+    return  redirect(url_for('adminDashboard'))
+
+@app.route('/bookoffline',methods = ['GET','POST'])
+@admin_login_required
+@login_required
+def offlineBooking():
+    orderId = str(uuid.uuid1().hex)
+    try:
+        newBooking = Bookings()
+        newBooking.user_id = g.user 
+        newBooking.order_id = orderId
+        newBooking.amount = 100
+        newBooking.txn_mode = "Offline"
+        db.session.add(newBooking)
+        db.session.commit()
+
+    except Exception as e:
+        print(e)
+
+    return render_template('offlinebooking.html',booking=newBooking)
 
 if __name__ == '__main__':
     app.run(debug=True ,port=8000)
--- models.py
+++ models.py
@@ -9,10 +9,17 @@ class Users(db.Model):
     lastname = db.Column(db.String(20))
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.Text, nullable=False)
+    isAdmin = db.Column(db.Boolean,nullable=False,default=False)
 
 class Bookings(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    booking_date = db.Column(db.Date, default = datetime.today())
+    booking_date = db.Column(db.DateTime, default = datetime.utcnow)
     status = db.Column(db.Boolean,nullable=False,default=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'),nullable=False)
+    order_id = db.Column(db.String(100), unique=True, nullable=False)
+    transaction_id = db.Column(db.String(100))
+    txn_mode = db.Column(db.String(20))
+    amount = db.Column(db.Integer)
 
+
+    
